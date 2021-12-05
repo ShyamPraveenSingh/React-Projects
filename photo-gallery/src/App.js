cce2ceb2456d308ce2b7aed285d2f37716866f34
ShyamPraveenSingh <shyampaveensingh@gmail.com>
2021-12-05 10:22:53 +0530

    first updates

---
+++ photo-gallery/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Images from "./Images";
+import "./styles.css";
+
+export default function App() {
+  const [selectedImg, setSelectedImg] = useState(Images[0]);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={selectedImg} alt="Selected" className="selected" />
+        <div className="imgContainer">
+          {Images.map((img, index) => (
+            <img
+              style={{ border: selectedImg === img ? "4px solid purple" : "" }}
+              key={index}
+              src={img}
+              alt="dog"
+              onClick={() => setSelectedImg(img)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ photo-gallery/src/Images.js
@@ -0,0 +1,13 @@
+export default [
+    "https://images.unsplash.com/photo-1560807707-8cc77767d783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+    "https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1524698604136-5a02fb1f7ec9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "https://images.unsplash.com/photo-1558236714-d1a6333fce68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1511732831640-a201294ac90e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "https://images.unsplash.com/photo-1544197807-bb503430e22d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1510337550647-e84f83e341ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80",
+    "https://images.unsplash.com/photo-1550948537-130a1ce83314?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80",
+    "https://images.unsplash.com/photo-1551239262-61cf00f7f55d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "https://images.unsplash.com/photo-1424709746721-b8fd0ff52499?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  ];
+  
