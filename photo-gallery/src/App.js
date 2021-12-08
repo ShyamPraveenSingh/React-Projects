5bbb289e878a9ff99983cbbe27fa13b16a5ac096
ShyamPraveenSingh <shyampaveensingh@gmail.com>
2021-12-08 16:05:37 +0530

    new

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import Images from "./Images";
 import "./styles.css";
 
-export default function App() {
+export default function App(){
   const [selectedImg, setSelectedImg] = useState(Images[0]);
+
 
   return (
     <div className="App">
@@ -11,15 +12,17 @@ export default function App() {
         <img src={selectedImg} alt="Selected" className="selected" />
         <div className="imgContainer">
           {Images.map((img, index) => (
-            <img
-              style={{ border: selectedImg === img ? "4px solid purple" : "" }}
-              key={index}
-              src={img}
-              alt="dog"
+            <img 
+              style={{ border: selectedImg === img ? "4px solid purple" : ""}} 
+              key={index} 
+              src={img} 
+              alt="dog" 
               onClick={() => setSelectedImg(img)}
-            />
+              />
           ))}
         </div>
+
+      
       </div>
     </div>
   );
